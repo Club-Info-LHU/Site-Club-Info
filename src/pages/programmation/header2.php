d386d83ece216c74eecb2505c19d88ef1fa9dc28
ProfViola <prof.viola@hotmail.fr>
2015-06-06 10:08:27 +0200

    Create header2.php

---
+++ src/pages/programmation/header2.php
@@ -0,0 +1,11 @@
+<header>
+	<h1>SITE CLUB INFORMATIQUE LHU</h1>
+</header>
+<nav> 
+	<ul> 
+		<li><a href="../../index.php">Accueil</a></li> 
+		<li><a href="../programmation.php">Programmation</a></li> 
+		<li><a href="../web.php">Langages Web</a></li> 
+		<li><a href="../concours.php">Concours</a></li> 
+	</ul> 
+</nav>
