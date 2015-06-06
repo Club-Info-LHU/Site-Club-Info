17eb83e1788b8de377c3c740a0b8252b163eef67
ProfViola <prof.viola@hotmail.fr>
2015-06-06 09:52:33 +0200

    Update and rename installation.html to installation.php

---
+++ src/pages/programmation/installation.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <?php include("../head.php"); ?>
+    
+    <body>
+        <?php include("../header.php"); ?>
+        
+        <section>
+            <article>
+                <h2>Python</h2>
+                
+                <p>
+                    <!-- installation du programme pour python -->
+                </p>
+            </article>
+            
+            <article>
+                <h2>C++</h2>
+                
+                <p>
+                    <!-- installation de code blocks -->
+                </p>
+            </article>
+        </section>
+        
+        <?php include("../footer.php"); ?>
+    </head>
+</html>
