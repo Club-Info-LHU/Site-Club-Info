60ce337f2f8cf83668b9e4424977cd2e7b2d6fa3
ProfViola <prof.viola@hotmail.fr>
2015-06-06 09:39:15 +0200

    Update header.php

--- src/pages/header.php
+++ src/pages/header.php
@@ -1,3 +1,11 @@
 <header>
 	<h1>SITE CLUB INFORMATIQUE LHU</h1>
 </header>
+<nav> 
+	<ul> 
+		<li><a href="index.html">Accueil</a></li> 
+		<li><a href="./pages/programmation.html">Programmation</a></li> 
+		<li><a href="./pages/web.html">Langages Web</a></li> 
+		<li><a href="./pages/concours.html">Concours</a></li> 
+	</ul> 
+</nav>
