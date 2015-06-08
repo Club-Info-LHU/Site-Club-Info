e25d285c123e4df4bf59c8b97b880ac7793e1422
ProfViola <prof.viola@hotmail.fr>
2015-06-08 09:58:47 +0200

    Update web.php

--- src/pages/web.php
+++ src/pages/web.php
@@ -23,60 +23,33 @@
             <article>
                 <h4>HTML</h4>
                 
-                <pre>
-<!DOCTYPE html>
-<html>
-    <head>
-        <?php include("head.php"); ?>
-        <title>Club Info LHU-Web</title> 
-    </head> 
-    
-    <body>
-        <?php include("header.php"); ?>
-        
-        <section>
-            <article>
-                <h2>Langages Web</h2>
-                <h3>Diviser pour mieux coder...</h3>
-                
                 <p>
-                    Nous verrons aussi comment concevoir des sites internet simples. 
-                    Pour cela, nous apprendrons à utiliser le HTML qui est un langage de balisage permettant de structurer le site 
-                    et le CSS qui gère la feuille de style de notre site, l'aspect esthétique.
+&lt;!DOCTYPE html&gt;</br>
+&lt;html&gt;</br>
+    &lt;head&gt;</br>
+        &lt;?php include("head.php"); ?&gt;</br>
+        &lt;title&gt;Club Info LHU-Web&lt;/title&gt;</br>
+    &lt;/head&gt;</br>
+    </br>
+    &lt;body&gt;</br>
+        &lt;?php include("header.php"); ?&gt;</br>
+        </br>
+        &lt;section&gt;</br>
+            &lt;article&gt;</br>
+                &lt;h2&gt;Langages Web&lt;/h2&gt;</br>
+                &lt;h3&gt;Diviser pour mieux coder...&lt;/h3&gt;</br>
+                </br>
+                &lt;p&gt;</br>
+                    Nous verrons aussi comment concevoir des sites internet simples. </br>
+                    Pour cela, nous apprendrons à utiliser le HTML qui est un langage de balisage permettant de structurer le site </br>
+                    et le CSS qui gère la feuille de style de notre site, l'aspect esthétique.</br>
+                &lt;/p&gt;</br>
+            &lt;/article&gt;</br>
+            </br>
+            &lt;article&gt;</br>
+                &lt;h4&gt;HTML&lt;/h4&gt;</br>
+                </br>
                 </p>
-            </article>
-            
-            <article>
-                <h4>HTML</h4>
-                
-                <pre>
-<!DOCTYPE html>
-<html>
-    <head>
-        <?php include("head.php"); ?>
-        <title>Club Info LHU-Web</title> 
-    </head> 
-    
-    <body>
-        <?php include("header.php"); ?>
-        
-        <section>
-            <article>
-                <h2>Langages Web</h2>
-                <h3>Diviser pour mieux coder...</h3>
-                
-                <p>
-                    Nous verrons aussi comment concevoir des sites internet simples. 
-                    Pour cela, nous apprendrons à utiliser le HTML qui est un langage de balisage permettant de structurer le site 
-                    et le CSS qui gère la feuille de style de notre site, l'aspect esthétique.
-                </p>
-            </article>
-            
-            <article>
-                <h4>HTML</h4>
-                </pre>
-
-                </pre>
             </article>
             
             <article>
