aef5367b21c96afd4352418e062340f93ec9e8f3
ProfViola <prof.viola@hotmail.fr>
2015-06-08 10:24:38 +0200

    Update web.php

--- src/pages/web.php
+++ src/pages/web.php
@@ -30,32 +30,32 @@
                 <p>
 &lt;!DOCTYPE html&gt;</br>
 &lt;html&gt;</br>
-    &lt;head&gt;</br>
-        &lt;?php include("head.php"); ?&gt; &#135;cette ligne est en php pour faciliter les modifications</br>
-        &lt;title&gt;Club Info LHU-Web&lt;/title&gt;</br>
-    &lt;/head&gt;</br>
+    &nbsp;&lt;head&gt;</br>
+        &nbsp;&nbsp;&lt;?php include("head.php"); ?&gt; &#135;cette ligne est en php pour faciliter les modifications</br>
+        &nbsp;&nbsp;&lt;title&gt;Club Info LHU-Web&lt;/title&gt;</br>
+    &nbsp;&lt;/head&gt;</br>
     </br>
-    &lt;body&gt;</br>
-        &lt;?php include("header.php"); ?&gt;</br>
+    &nbsp;&lt;body&gt;</br>
+        &nbsp;&nbsp;&lt;?php include("header.php"); ?&gt;</br>
         </br>
-        &lt;section&gt;</br>
-            &lt;article&gt;</br>
-                &lt;h2&gt;Langages Web&lt;/h2&gt;</br>
-                &lt;h3&gt;Diviser pour mieux coder...&lt;/h3&gt;</br>
+        &nbsp;&nbsp;&lt;section&gt;</br>
+            &nbsp;&nbsp;&nbsp;&lt;article&gt;</br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&lt;h2&gt;Langages Web&lt;/h2&gt;</br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&lt;h3&gt;Diviser pour mieux coder...&lt;/h3&gt;</br>
                 </br>
-                &lt;p&gt;</br>
-                    Nous verrons aussi comment concevoir des sites internet simples. 
-                    Pour cela, nous apprendrons à utiliser le HTML qui est un langage de balisage permettant de structurer le site ,
-                    un peu de PHP pour l'insertion de codes qui se répète de pages en pages 
-                    et le CSS qui gère la feuille de style de notre site, l'aspect esthétique.
-                &lt;/p&gt;</br>
-            &lt;/article&gt;</br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&lt;p&gt;</br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nous verrons aussi comment concevoir des sites internet simples. 
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pour cela, nous apprendrons à utiliser le HTML qui est un langage de balisage permettant de structurer le site ,
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;un peu de PHP pour l'insertion de codes qui se répète de pages en pages 
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;et le CSS qui gère la feuille de style de notre site, l'aspect esthétique.
+                &nbsp;&nbsp;&nbsp;&nbsp;&lt;/p&gt;</br>
+            &nbsp;&nbsp;&nbsp;&lt;/article&gt;</br>
             </br>
-            &lt;article&gt;</br>
-                &lt;h4&gt;HTML et PHP&lt;/h4&gt;</br>
-                &lt;p&gt;
-                    Voici par exemple les premières lignes de codes HTML et PHP correspondant à cette page.
-                &lt;/p&gt;
+            &nbsp;&nbsp;&nbsp;&lt;article&gt;</br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&lt;h4&gt;HTML et PHP&lt;/h4&gt;</br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&lt;p&gt;
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Voici par exemple les premières lignes de codes HTML et PHP correspondant à cette page.
+                &nbsp;&nbsp;&nbsp;&nbsp;&lt;/p&gt;
                 </br>
                 </p>
             </article>
