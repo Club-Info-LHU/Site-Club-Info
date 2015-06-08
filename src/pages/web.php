b85f1ad7b64d60e6ebb54d5a76ef4b99a66a8124
ProfViola <prof.viola@hotmail.fr>
2015-06-08 10:04:24 +0200

    Update web.php

--- src/pages/web.php
+++ src/pages/web.php
@@ -15,7 +15,8 @@
                 
                 <p>
                     Nous verrons aussi comment concevoir des sites internet simples. 
-                    Pour cela, nous apprendrons à utiliser le HTML qui est un langage de balisage permettant de structurer le site 
+                    Pour cela, nous apprendrons à utiliser le HTML qui est un langage de balisage permettant de structurer le site, 
+                    un peu de PHP pour l'insertion de codes qui se répète de pages en pages 
                     et le CSS qui gère la feuille de style de notre site, l'aspect esthétique.
                 </p>
             </article>
@@ -27,7 +28,7 @@
 &lt;!DOCTYPE html&gt;</br>
 &lt;html&gt;</br>
     &lt;head&gt;</br>
-        &lt;?php include("head.php"); ?&gt;</br>
+        &lt;?php include("head.php"); ?&gt; &#135;cette ligne est en php pour faciliter les modifications</br>
         &lt;title&gt;Club Info LHU-Web&lt;/title&gt;</br>
     &lt;/head&gt;</br>
     </br>
@@ -42,6 +43,7 @@
                 &lt;p&gt;</br>
                     Nous verrons aussi comment concevoir des sites internet simples. 
                     Pour cela, nous apprendrons à utiliser le HTML qui est un langage de balisage permettant de structurer le site 
+                    un peu de PHP pour l'insertion de codes qui se répète de pages en pages 
                     et le CSS qui gère la feuille de style de notre site, l'aspect esthétique.
                 &lt;/p&gt;</br>
             &lt;/article&gt;</br>
