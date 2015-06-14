762525ba0a75cf7c58030d18c57e715ae1f90081
ProfViola <prof.viola@hotmail.fr>
2015-06-14 18:52:25 +0200

    Update head.php

--- src/pages/head.php
+++ src/pages/head.php
@@ -1,1 +1,6 @@
 <meta charset="utf-8"/> 
+<link rel="stylesheet" href="../css/style.css" />
+<link rel="stylesheet" href="./css/style.css" />
+<!--[if lt IE 9]>
+    <script src="http://github.com/aFarkas/html5shiv/blob/master/dist/html5shiv.js"></script>
+<![endif]-->
