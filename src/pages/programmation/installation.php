c44fc27dc194d48e05e2dabcaafaf3ab80bb2f3c
Ceytix <ceytix@mailoo.org>
2015-06-28 17:24:40 +0200

    installation d'anaconda

--- src/pages/programmation/installation.php
+++ src/pages/programmation/installation.php
@@ -13,7 +13,14 @@
                 <h2>Python</h2>
                 
                 <p>
-                    <!-- installation du programme pour python -->
+                    Si vous souhaitez pouvoir programmer chez vous, il est préférable de disposer d'un environnement de 
+                    développement. <br/>
+                    Pour Python, nous vous proposons Anaconda, que vous pouvez télécharger <a href="http://continuum.io/downloads">ici</a>.<br/>
+                    Tout d'abord, sélectionnez votre système d'exploitation, puis sélectionnez l'installation en 32 ou en 64 bit.<br/>
+                    <img src="../../pictures/anaconda.png" alt="capture d'écran de l'installation d'Anaconda" />
+                    Si vous êtes sous Windows, lancez le .exe que vous venez de télécharger et suivez les instructions affichées.<br/>
+                    Sous Mac ou linux, lancez le terminal, allez dans le dossier dans lequel se trouve Anaconda-2.2.0-Linux-x86_64.sh et rentrez la commande 
+                    <b>bash Anaconda-2.2.0-Linux-x86_64.sh</b> puis suivez les instructions affichées sur le terminal.<br/>                   
                 </p>
             </article>
             
